97ce81373128df61c3b4d4f328b727b76a17b379
leafyzito <rafaelzao2003@gmail.com>
2024-09-19 01:07:04 +0100

    added whisper message to `join`

--- commands/join/join.js
+++ commands/join/join.js
@@ -53,6 +53,7 @@ const joinCommand = async (client, message) => {
 
     const happyEmote = await client.emotes.getEmoteFromList(message.channelName, client.emotes.happyEmotes);
     client.log.logAndReply(message, `Entrei no chat ${message.senderUsername} com sucesso! Tô lá te esperando! ${happyEmote} Caso tenha follow-mode ativado, me dê cargo de moderador no seu chat para conseguir falar lá`);
+    client.log.logAndWhisper(message, `Caso tenha follow-mode ativado no seu chat, me dê cargo de moderador ou vip para conseguir falar lá`);
     return;
 };
 
@@ -63,7 +64,7 @@ joinCommand.cooldown = 5000;
 joinCommand.whisperable = true;
 joinCommand.description = `Utilize o comando !join num chat no qual o Folhinha esteja presente e faça com que o bot entre no chat de quem executou o comando
 
-Caso tenha follow-mode ativado no seu chat, o bot não conseguirá falar no seu chat. Para resolver isso, dê cargo de moderador ao Folhinha`;
+Caso tenha follow-mode ativado no seu chat, o bot não conseguirá falar no seu chat. Para resolver isso, dê cargo de moderador ou vip ao Folhinha`;
 joinCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/commands/${joinCommand.commandName}/${joinCommand.commandName}.js`;
 
 module.exports = {
