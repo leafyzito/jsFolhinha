e0437175f61c820535d9c26c15d9dd0bb85e24d8
leafyzito <rafaelzao2003@gmail.com>
2024-10-12 02:21:38 +0100

    remove temporary afk status

--- commands/afk/afk_info_model.js
+++ commands/afk/afk_info_model.js
@@ -24,14 +24,6 @@ class AFKInfo {
 const afkData =
     [
         {
-            "alias": ["montarpcgamerpoggers"],
-            "emoji": "😎🖥️",
-            "afk": "foi montar o seu novo pc gamer poggers demais",
-            "isafk": "montando o seu novo pc gamer poggers demais",
-            "returned": "montou o seu novo pc gamer poggers demais",
-            "rafk": "montando o seu novo pc gamer poggers demais"
-        },
-        {
             "alias": ["afk"],
             "emoji": "🏃⌨",
             "afk": "ficou afk",
