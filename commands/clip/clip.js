9962f0ffc17515fe7bd1174c37e647e1431d3020
leafyzito <rafaelzao2003@gmail.com>
2024-11-15 22:35:56 +0000

    if creating clips is forbidden, create my own

--- commands/clip/clip.js
+++ commands/clip/clip.js
@@ -1,6 +1,73 @@
 const { processCommand } = require("../../utils/processCommand.js");
+const path = require('path');
+const fs = require('fs');
+const FormData = require('form-data');
+const fetch = require('node-fetch');
 
-async function creteTwitchClip(channelId) {
+async function uploadToFeridinha(content, clipName) {
+    const api_url = 'https://feridinha.com/upload';
+    const headers = { 'token': process.env.FERIDINHA_API_KEY };
+
+    const form = new FormData();
+    form.append('file', content, {
+        filename: clipName,
+        contentType: 'video/mp4'
+    });
+
+    try {
+        const response = await fetch(api_url, {
+            method: 'POST',
+            headers: {
+                'token': headers.token,
+                ...form.getHeaders()
+            },
+            body: form
+        });
+
+        if (response.ok) {
+            const resData = await response.json();
+            if (resData.success) {
+                return resData.message;
+            }
+            console.log(`Failed to upload to feridinha. resData: ${JSON.stringify(resData)}`);
+        } else {
+            console.log(`Error: ${response.status} ${response.statusText}`);
+            const errorText = await response.text();
+            console.log(`Error details: ${errorText}`);
+        }
+    } catch (error) {
+        console.log('Upload error:', error);
+    }
+    return null;
+}
+
+async function makeClip(channelName) {
+    try {
+        const response = await fetch(`http://localhost:8989/clip/${channelName}`);
+        const data = await response.json();
+
+        if (!response.ok) {
+            return null;
+        }
+
+        // Resolve the path to the "clips" folder inside "twitchClipper"
+        const clipsFolder = path.resolve(__dirname, '..', '..', '..', '..', 'twitchClipper', 'clips');
+        var clipPath = path.join(clipsFolder, data.path);
+
+        // upload clip to feridinha
+        const clipName = path.basename(clipPath);
+        const clipContent = fs.readFileSync(clipPath);
+        const clipUrl = await uploadToFeridinha(clipContent, clipName);
+
+        return clipUrl;
+
+    } catch (error) {
+        console.log('Error making clip:', error);
+        return null;
+    }
+}
+
+async function createTwitchClip(channelId, channelName) {
     const api_url = `https://api.twitch.tv/helix/clips?broadcaster_id=${channelId}&has_delay=true`;
     const response = await fetch(api_url, {
         method: 'POST',
@@ -8,7 +75,13 @@ async function creteTwitchClip(channelId) {
     });
 
     const data = await response.json();
-    if (data.status === 403) { return 'forbidden'; };
+    if (data.status === 403) {
+        const createdClip = await makeClip(channelName);
+        if (!createdClip) {
+            return 'forbidden';
+        }
+        return createdClip;
+    };
     if (data.status === 404) { return null; };
 
     const clipId = data.data[0].id;
@@ -35,7 +108,7 @@ const clipCommand = async (client, message) => {
         return;
     }
 
-    const clip = await creteTwitchClip(targetId);
+    const clip = await createTwitchClip(targetId, targetChannel);
     if (!clip) {
         client.log.logAndReply(message, `O canal ${targetChannel} não está em live`);
         return;
@@ -44,8 +117,12 @@ const clipCommand = async (client, message) => {
         client.log.logAndReply(message, `Esse canal não permite criar clipes`);
         return;
     }
-    client.log.logAndReply(message, `🎬 https://clips.twitch.tv/${clip.id}`)
-    return;
+    if (clip.id) {
+        client.log.logAndReply(message, `🎬 https://clips.twitch.tv/${clip.id}`)
+        return;
+    }
+    client.log.logAndReply(message, `🎬 ${clip}`)
+
 };
 
 clipCommand.commandName = 'clip';
