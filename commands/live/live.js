f092a49ff5a106a3d3bc549a184c25843237541c
leafyzito <rafaelzao2003@gmail.com>
2024-11-18 22:22:29 +0000

    fix to `live`

--- commands/live/live.js
+++ commands/live/live.js
@@ -21,7 +21,7 @@ async function getLive(liveTarget) {
         return {
             isLive: true,
             title: data[0].stream.title,
-            game: data[0].stream.game.displayName,
+            game: data[0].stream.game.displayName || '(sem categoria)',
             viewers: data[0].stream.viewersCount,
             startedAt: data[0].stream.createdAt,
         };
