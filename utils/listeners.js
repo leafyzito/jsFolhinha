75b6927e8a33c9f3cb6f682b5f42c3d4e2dece53
leafyzito <rafaelzao2003@gmail.com>
2024-12-10 07:20:41 +0000

    rephrasing

--- utils/listeners.js
+++ utils/listeners.js
@@ -98,7 +98,7 @@ const reminderListener = async (client, message) => {
         }
 
         if (replyMsg.length > 480) {
-            client.log.send(message.channelName, `${message.senderUsername}, você tem ${reminders.length} lembretes pendentes. Use o comando ${message.commandPrefix}remind <show> para ver os IDs dos lembretes`);
+            client.log.send(message.channelName, `${message.senderUsername}, você tem ${reminders.length} lembretes pendentes. Use o comando "${message.commandPrefix}remind show" para ver os IDs dos lembretes`);
             client.notifiedUsers.push(message.senderUserID);
             processingReminder = processingReminder.filter(user => user !== message.senderUsername);
             return;
@@ -116,7 +116,7 @@ const reminderListener = async (client, message) => {
         return;
     }
 
-    client.log.send(message.channelName, `${message.senderUsername}, você tem ${reminders.length} lembretes pendentes. Use o comando ${message.commandPrefix}remind <show> para ver os IDs dos lembretes. Pode também usar ${message.commandPrefix}remind <show all> para ver todos os lembretes de uma vez`);
+    client.log.send(message.channelName, `${message.senderUsername}, você tem ${reminders.length} lembretes pendentes. Use o comando "${message.commandPrefix}remind show" para ver os IDs dos lembretes. Pode também usar "${message.commandPrefix}remind show all" para ver todos os lembretes de uma vez`);
     client.notifiedUsers.push(message.senderUserID);
     processingReminder = processingReminder.filter(user => user !== message.senderUsername);
     return;
