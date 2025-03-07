011c8083f15c908f5e99cb5e4f7c5dc7faa63fda
leafyzito <rafaelzao2003@gmail.com>
2025-03-07 07:48:55 +0000

    Add Justlog channel integration to join command and task

---
+++ utils/justlog.js
@@ -0,0 +1,23 @@
+async function addChannelToJustlog(client, channelId) {
+    const response = await fetch(`http://localhost:8025/admin/channels`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': `${process.env.JUSTLOG_API_KEY}`
+        },
+        body: JSON.stringify({
+            channels: channelId
+        })
+    });
+
+    if (response.status !== 200) {
+        client.discord.importantLog(`Erro ao adicionar ${channelId} ao justlog: ${response.statusText}`);
+        return false;
+    }
+
+    return true;
+}
+
+module.exports = {
+    addChannelToJustlog
+}
