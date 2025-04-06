38970e48c952f349eb6e4ac298ec072e82375391
leafyzito <rafaelzao2003@gmail.com>
2025-04-06 14:06:44 +0100

    refactor(firstline): restructure first line command

--- commands/firstline/firstline.js
+++ commands/firstline/firstline.js
@@ -1,60 +1,90 @@
-// const { processCommand } = require("../../utils/processCommand.js");
-// const { timeSince } = require("../../utils/utils.js");
+const { processCommand } = require("../../utils/processCommand.js");
+const { timeSince } = require("../../utils/utils.js");
 
-// async function getFirstLine(userid, channelid) {
-//     const url = `http://localhost:8025/channelid/${channelid}/userid/${userid}/firstMsg`
-//     const headers = { accept: 'application/json' }
-//     const response = await fetch(url, { headers })
-//     // console.log(data)
-//     if (response.status !== 200) {
-//         return null;
-//     }
-//     const data = await response.text()
-//     // console.log(data)
-//     // Get only the first line from the multi-line response
-//     const firstLine = data.split('\n')[0];
-//     // Parse the log line into components
-//     const regex = /\[(.*?)\] #(.*?) (.*?): (.*)/;
-//     const [_, timestamp, channelName, user, message] = firstLine.match(regex);
+async function getFirstLineDate(channelid, usernameid) {
+    const apiUrl = `http://localhost:8025/list?channelid=${channelid}&userid=${usernameid}`;
 
-//     return {
-//         channel: channelName,
-//         user: user,
-//         message: message,
-//         date: timestamp
-//     }
-// }
+    const response = await fetch(apiUrl, {
+        method: 'GET',
+    });
 
-// const firstLineCommand = async (client, message) => {
-//     message.command = 'firstline';
-//     if (!await processCommand(5000, 'channel', message, client)) return;
+    if (!response.ok) {
+        // console.log('Error fetching logs:', response.statusText);
+        return null;
+    }
 
-//     let targetUser = message.messageText.split(' ')[1]?.replace(/^@/, '') || message.senderUsername;
-//     const targetId = targetUser !== message.senderUsername.toLowerCase() ? await client.getUserID(targetUser.toLowerCase()) : message.senderUserID;
+    const data = await response.json();
+
+    const availableLogsLen = data.availableLogs.length;
+    const firstAvailableLog = data.availableLogs[availableLogsLen - 1];
+
+    const logUrl = `http://localhost:8025/channelid/${channelid}/userid/${usernameid}/${firstAvailableLog.year}/${firstAvailableLog.month}`;
+
+    return logUrl;
+}
+
+async function getFirstLineMessage(logUrl) {
+    const response = await fetch(logUrl, {
+        method: 'GET',
+    });
+
+    if (!response.ok) {
+        // console.log('Error fetching logs:', response.statusText);
+        return null;
+    }
+
+    const data = await response.text();
+    const firstLine = data.split('\n')[0];
+
+    const regex = /\[(.*?)\] #(.*?) (.*?): (.*)/;
+    const [_, timestamp, channelName, user, message] = firstLine.match(regex);
+    return {
+        date: timestamp,
+        channel: channelName,
+        user: user,
+        message: message
+    };
+}
+
+async function getFirstLine(channelid, userid) {
+    const firstLogUrl = await getFirstLineDate(channelid, userid);
+    if (!firstLogUrl) {
+        return null;
+    }
+    const firstLineMessage = await getFirstLineMessage(firstLogUrl);
+    return firstLineMessage;
+}
+
+const firstLineCommand = async (client, message) => {
+    message.command = 'firstline';
+    if (!await processCommand(5000, 'channel', message, client)) return;
+
+    let targetUser = message.messageText.split(' ')[1]?.replace(/^@/, '') || message.senderUsername;
+    const targetId = targetUser !== message.senderUsername.toLowerCase() ? await client.getUserID(targetUser.toLowerCase()) : message.senderUserID;
 
 
-//     const firstLine = await getFirstLine(targetId, message.channelID);
-//     if (!firstLine) {
-//         client.log.logAndReply(message, `Nunca loguei uma mensagem desse usuário neste chat (contando desde 06/03/2025)`);
-//         return;
-//     }
+    const firstLine = await getFirstLine(message.channelID, targetId);
+    if (!firstLine) {
+        client.log.logAndReply(message, `Nunca loguei uma mensagem desse usuário neste chat (contando desde 06/03/2025)`);
+        return;
+    }
 
-//     client.log.logAndReply(message, `${targetUser === message.senderUsername.toLowerCase() ? 'A sua primeira' : `A primeira mensagem de ${firstLine.user}`} neste chat foi em ${firstLine.date}: ${firstLine.message}`);
-//     return;
-// };
+    client.log.logAndReply(message, `${targetUser === message.senderUsername.toLowerCase() ? 'A sua primeira' : `A primeira mensagem de ${firstLine.user}`} neste chat foi em ${firstLine.date}: ${firstLine.message}`);
+    return;
+};
 
-// firstLineCommand.commandName = 'firstline';
-// firstLineCommand.aliases = ['firstline', 'fl'];
-// firstLineCommand.shortDescription = 'Veja a primeira mensagem de um usuário';
-// firstLineCommand.cooldown = 5000;
-// firstLineCommand.whisperable = false;
-// firstLineCommand.description = `Receba a primeira mensagem de um usuário fornecido ou, caso nenhum seja fornecido, da pessoa que executou o comando
-// • Exemplo: !firstline - O bot vai mostrar a primeira mensagem de quem executou o comando no chat onde o comando foi executado
-// • Exemplo: !firstline @leafyzito - O bot vai mostrar a primeira mensagem de @leafyzito no chat onde o comando foi executado
+firstLineCommand.commandName = 'firstline';
+firstLineCommand.aliases = ['firstline', 'fl'];
+firstLineCommand.shortDescription = 'Veja a primeira mensagem de um usuário';
+firstLineCommand.cooldown = 5000;
+firstLineCommand.whisperable = false;
+firstLineCommand.description = `Receba a primeira mensagem de um usuário fornecido ou, caso nenhum seja fornecido, da pessoa que executou o comando
+• Exemplo: !firstline - O bot vai mostrar a primeira mensagem de quem executou o comando no chat onde o comando foi executado
+• Exemplo: !firstline @leafyzito - O bot vai mostrar a primeira mensagem de @leafyzito no chat onde o comando foi executado
 
-// Começou a contar desde 06/03/2025`;
-// firstLineCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/commands/${firstLineCommand.commandName}/${firstLineCommand.commandName}.js`;
+Começou a contar desde 06/03/2025`;
+firstLineCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/commands/${firstLineCommand.commandName}/${firstLineCommand.commandName}.js`;
 
-// module.exports = {
-//     firstLineCommand,
-// };
+module.exports = {
+    firstLineCommand,
+};
