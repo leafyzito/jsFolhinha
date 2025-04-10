e4dfbeac2b237332f4b72939c1014dfc80c9116f
leafyzito <rafaelzao2003@gmail.com>
2025-04-10 20:29:33 +0100

    refactor(nick): use data from the api only

--- commands/nick/nick.js
+++ commands/nick/nick.js
@@ -1,7 +1,8 @@
 const { processCommand } = require("../../utils/processCommand.js");
+const { manageLongResponse } = require("../../utils/utils.js");
 
 async function getAllNicks(userId) {
-    const api_url = `https://logs.zonian.dev/namehistory/${userId}`;
+    const api_url = `https://logs.spanix.team/namehistory/${userId}`;
     const response = await fetch(api_url);
     const data = await response.json();
 
@@ -14,32 +15,38 @@ const nicksCommand = async (client, message) => {
     if (!await processCommand(5000, 'channel', message, client)) return;
 
     const nicksTarget = message.messageText.split(' ').slice(1).find(arg => !arg.startsWith('-'))?.replace(/^@/, '').toLowerCase() || message.senderUsername;
-    const userAliases = await client.db.get('users', { aliases: nicksTarget });
+    const targetId = nicksTarget === message.senderUsername ? message.senderID : await client.getUserID(nicksTarget);
+    const allNicks = await getAllNicks(targetId);
+    const aliases = allNicks.join(' → ');
 
     // if any of the message args is "-all" or "-todos", get all nicks
-    if (message.messageText.includes('-all') || message.messageText.includes('-todos')) {
-        console.log(nicksTarget);
-        const targetId = await client.getUserID(nicksTarget);
-        if (!targetId) {
-            client.log.logAndReply(message, `Esse usuário não existe`);
-            return;
-        }
-        const allNicks = await getAllNicks(targetId);
-        const aliases = allNicks.join(' → ');
-        client.log.logAndReply(message, `O histórico de nicks de ${nicksTarget} (id: ${targetId}) é: ${aliases}`);
-        return;
-    }
+    // if (message.messageText.includes('-all') || message.messageText.includes('-todos')) {
+    //     console.log(nicksTarget);
+    //     const targetId = await client.getUserID(nicksTarget);
+    //     if (!targetId) {
+    //         client.log.logAndReply(message, `Esse usuário não existe`);
+    //         return;
+    //     }
+    //     const allNicks = await getAllNicks(targetId);
+    //     const aliases = allNicks.join(' → ');
+    //     client.log.logAndReply(message, `O histórico de nicks de ${nicksTarget} (id: ${targetId}) é: ${aliases}`);
+    //     return;
+    // }
 
-    if (userAliases.length === 0) {
-        client.log.logAndReply(message, `Nunca vi esse usuário`);
-        return;
-    }
+    // if (userAliases.length === 0) {
+    //     client.log.logAndReply(message, `Nunca vi esse usuário`);
+    //     return;
+    // }
 
-    const aliases = userAliases[0].aliases.join(' → ');
-    const idFromDb = userAliases[0].userid;
-    client.log.logAndReply(message,
-        `${nicksTarget === message.senderUsername ? `O seu histórico de nicks é:`
-            : `O histórico de nicks de ${nicksTarget} (id: ${idFromDb}) é:`} ${aliases}`);
+    // const aliases = userAliases[0].aliases.join(' → ');
+    // const idFromDb = userAliases[0].userid;
+
+    let response = `${nicksTarget === message.senderUsername ? `O seu histórico de nicks é:` : `O histórico de nicks de ${nicksTarget} (id: ${targetId}) é:`} ${aliases}`
+    if (response.length > 490) {
+        response = await manageLongResponse(response);
+    }
+    client.log.logAndReply(message, response);
+
 };
 
 nicksCommand.commandName = 'nick';
@@ -47,12 +54,10 @@ nicksCommand.aliases = ['nick', 'nicks', 'nicknames', 'namehistory', 'nickhistor
 nicksCommand.shortDescription = 'Mostra o histórico de nicks de algum usuário';
 nicksCommand.cooldown = 5000;
 nicksCommand.whisperable = true;
-nicksCommand.description = `Exibe o histórico de nicks de um usuário ou de quem executou o comando caso nenhum usuário seja fornecido (desde que o usuário tenha sido visto pelo bot)
-• Exemplo: !nicks @leafyzito - O bot irá responder com o histórico de nicks de leafyzito, desde a partir do momento que ele foi visto pelo bot
-
-É também possível obter todo (ou quase todo) o histórico de nicks de um usuário usando o argumento -all ou -todos. Note que esta versão do comando pode ser um pouco lenta
-• Exemplo: !nicks @leafyzito -all - O bot irá responder com o histórico de nicks de leafyzito, consoante a API usada
-`;
+nicksCommand.description = `Exibe o histórico de nicks de um usuário ou de quem executou o comando caso nenhum usuário seja fornecido
+• Exemplo: !nicks @leafyzito - O bot irá responder com o histórico de nicks de leafyzito`;
+// É também possível obter todo (ou quase todo) o histórico de nicks de um usuário usando o argumento -all ou -todos. Note que esta versão do comando pode ser um pouco lenta
+// • Exemplo: !nicks @leafyzito -all - O bot irá responder com o histórico de nicks de leafyzito, consoante a API usada
 nicksCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/commands/${nicksCommand.commandName}/${nicksCommand.commandName}.js`;
 
 module.exports = {
