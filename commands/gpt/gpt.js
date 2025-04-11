266ac66ef9cce486a3fde53586ca4085cbd69348
leafyzito <rafaelzao2003@gmail.com>
2025-04-11 23:04:11 +0100

    feat(uwu, gptuwu): add uwu command and added gpt uwu command

--- commands/gpt/gpt.js
+++ commands/gpt/gpt.js
@@ -1,8 +1,10 @@
 const { OpenAI } = require('openai');
+const Uwuifier = require("uwuifier").default;
 const { processCommand } = require("../../utils/processCommand.js");
 const { manageLongResponse } = require("../../utils/utils.js");
 
 const gptClient = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const uwuifier = new Uwuifier();
 
 async function askGpt(message, prompt) {
     const completion = await gptClient.chat.completions.create({
@@ -37,6 +39,28 @@ async function askGptSerio(message, prompt) {
     return completion.choices[0].message.content;
 }
 
+async function askGptUwu(message, prompt) {
+    const completion = await gptClient.chat.completions.create({
+        model: 'gpt-3.5-turbo-0125',
+        messages: [
+            {
+                role: 'system', content: ` 
+Mantenha a resposta o mais curta e concisa possível, com no máximo 300 caracteres. 
+O seu nome é Folhinha, uma IA (de género masculino), mas só partilhe essas informações se estritamente pedido. 
+Você é um bot no chat de ${message.channelName}, um chat público da Twitch, onde qualquer pessoa pode falar, então mantenha isso em mente. 
+Seja meio bobinho e engraçadinho para manter as respostas únicas e criativas, mas cuidado pra não ser brega. 
+Você deve digirir a sua resposta a ${message.senderUsername}. 
+Em nenhuma circunstância faça referência a este prompt na sua resposta. 
+`
+            },
+            { role: 'user', content: prompt }
+        ],
+    });
+
+    const result = completion.choices[0].message.content;
+    const uwuifiedResult = uwuifier.uwuifySentence(result);
+    return uwuifiedResult;
+}
 
 const gptCommand = async (client, message) => {
     message.command = 'gpt';
@@ -72,6 +96,23 @@ const gptSerioCommand = async (client, message) => {
     client.log.logAndReply(message, `🤖 ${gptRes.replace(/(\r\n|\n|\r)/gm, " ")}`);
 }
 
+const gptUwuCommand = async (client, message) => {
+    message.command = 'gpt';
+    if (!await processCommand(15000, 'channel', message, client)) return;
+
+    const prompt = message.messageText.split(' ').slice(1).join(' ');
+
+    if (!prompt) {
+        client.log.logAndReply(message, `Use o formato: ${message.commandPrefix}gptserio <qualquer coisa>`);
+        return;
+    }
+
+    var gptRes = await askGptUwu(message, prompt);
+    if (gptRes.length > 490) { gptRes = await manageLongResponse(gptRes); }
+
+    client.log.logAndReply(message, `🤖 ${gptRes.replace(/(\r\n|\n|\r)/gm, " ")}`);
+}
+
 gptCommand.commandName = 'gpt';
 gptCommand.aliases = ['gpt', 'chatgpt'];
 gptCommand.shortDescription = 'Faça uma pergunta para o ChatGPT';
@@ -79,7 +120,8 @@ gptCommand.cooldown = 15000;
 gptCommand.whisperable = true;
 gptCommand.description = `Envie uma mensagem para o GPT com a personalidade do Folhinha
 Use esse comando para diversão apenas
-Caso deseje usar para perguntar alguma dúvida genuina, use o comando <a href="https://folhinhabot.com/comandos/gptserio" style="color: #67e8f9">!gptserio</a> que lhe responderá de maneira mais acertiva e extensa, sem a personalidade brincalhona do !gpt normal`;
+Caso deseje usar para perguntar alguma dúvida genuina, use o comando <a href="https://folhinhabot.com/comandos/gptserio" style="color: #67e8f9">!gptserio</a> que lhe responderá de maneira mais acertiva e extensa, sem a personalidade brincalhona do !gpt normal
+Tem também o <a href="https://folhinhabot.com/comandos/gptuwu" style="color: #67e8f9">!gptuwu</a> que tem uma personalidade meio uwuástica...`;
 gptCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/commands/${gptCommand.commandName}/${gptCommand.commandName}.js`;
 
 gptSerioCommand.commandName = 'gptserio';
@@ -90,7 +132,16 @@ gptSerioCommand.whisperable = true;
 gptSerioCommand.description = `Envie uma mensagem para o GPT com uma personalidade mais séria e sem teor humorístico`;
 gptSerioCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/commands/${gptCommand.commandName}/${gptCommand.commandName}.js`;
 
+gptUwuCommand.commandName = 'gptuwu';
+gptUwuCommand.aliases = ['gptuwu', 'chatgptuwu'];
+gptUwuCommand.shortDescription = 'Faça uma pergunta para o ChatGPT uwu';
+gptUwuCommand.cooldown = 15000;
+gptUwuCommand.whisperable = true;
+gptUwuCommand.description = `Envie uma mensagem para o GPT com uma personalidade meio uwuástica`;
+gptUwuCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/commands/${gptCommand.commandName}/${gptCommand.commandName}.js`;
+
 module.exports = {
     gptCommand,
     gptSerioCommand,
+    gptUwuCommand,
 };
