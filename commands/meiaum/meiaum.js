479255c92a09d3477347102dce67d67108ca9b95
leafyzito <rafaelzao2003@gmail.com>
2025-05-15 09:06:53 +0100

    disable(omeiaum): o7 meiathon

--- commands/meiaum/meiaum.js
+++ commands/meiaum/meiaum.js
@@ -1,61 +1,64 @@
-const { processCommand } = require("../../utils/processCommand.js");
+// o7
 
-function getFormattedRemainingTime(seconds) {
-    // convert from ms to seconds
-    seconds = seconds / 1000;
-    if (seconds < 60) {
-        return `${seconds}s`;
-    }
 
-    const days = (seconds / 86400).toFixed(2);
-    const minutes = Math.floor(seconds / 60);
-    if (minutes < 60) {
-        const secondsLeft = seconds % 60;
-        return `${minutes}m ${secondsLeft}s`;
-    }
+// const { processCommand } = require("../../utils/processCommand.js");
 
-    const hours = Math.floor(minutes / 60);
-    const minutesLeft = minutes % 60;
-    const secondsLeft = seconds % 60;
+// function getFormattedRemainingTime(seconds) {
+//     // convert from ms to seconds
+//     seconds = seconds / 1000;
+//     if (seconds < 60) {
+//         return `${seconds}s`;
+//     }
 
-    return `${hours}h ${minutesLeft}m ${Math.round(secondsLeft)}s (${days} dias)`;
-}
+//     const days = (seconds / 86400).toFixed(2);
+//     const minutes = Math.floor(seconds / 60);
+//     if (minutes < 60) {
+//         const secondsLeft = seconds % 60;
+//         return `${minutes}m ${secondsLeft}s`;
+//     }
 
-async function getMeiaUm() {
-    const api_url = "https://subathon-api.justdavi.dev/api/time-left";
-    const response = await fetch(api_url);
-    if (!response.ok) {
-        return null;
-    };
+//     const hours = Math.floor(minutes / 60);
+//     const minutesLeft = minutes % 60;
+//     const secondsLeft = seconds % 60;
 
-    const data = await response.json();
-    const msTimeLeft = data.timeLeft;
-    return getFormattedRemainingTime(msTimeLeft);
-}
+//     return `${hours}h ${minutesLeft}m ${Math.round(secondsLeft)}s (${days} dias)`;
+// }
 
-const meiaUmCommand = async (client, message) => {
-    message.command = 'meiaum';
-    if (!await processCommand(5000, 'channel', message, client)) return;
+// async function getMeiaUm() {
+//     const api_url = "https://subathon-api.justdavi.dev/api/time-left";
+//     const response = await fetch(api_url);
+//     if (!response.ok) {
+//         return null;
+//     };
 
-    const timeLeft = await getMeiaUm();
-    if (!timeLeft) {
-        client.log.logAndReply(message, `A API do timer da subathon está morta, tente novamente mais tarde`);
-        return;
-    }
+//     const data = await response.json();
+//     const msTimeLeft = data.timeLeft;
+//     return getFormattedRemainingTime(msTimeLeft);
+// }
 
-    const emote = await client.emotes.getEmoteFromList(message.channelName, ['meiaThon'], '');
-    client.log.logAndReply(message, `Timer atual da subathon do MeiaUm: ${timeLeft} ${emote}`);
-    return;
-};
+// const meiaUmCommand = async (client, message) => {
+//     message.command = 'meiaum';
+//     if (!await processCommand(5000, 'channel', message, client)) return;
 
-meiaUmCommand.commandName = 'meiaum';
-meiaUmCommand.aliases = ['omeiaum', 'meiaum', 'meiaumtimer'];
-meiaUmCommand.shortDescription = 'Veja quanto tempo de live o meiaum ainda tem pela frente';
-meiaUmCommand.cooldown = 5000;
-meiaUmCommand.whisperable = true;
-meiaUmCommand.description = ``;
-meiaUmCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/commands/${meiaUmCommand.commandName}/${meiaUmCommand.commandName}.js`;
+//     const timeLeft = await getMeiaUm();
+//     if (!timeLeft) {
+//         client.log.logAndReply(message, `A API do timer da subathon está morta, tente novamente mais tarde`);
+//         return;
+//     }
 
-module.exports = {
-    meiaUmCommand,
-};
+//     const emote = await client.emotes.getEmoteFromList(message.channelName, ['meiaThon'], '');
+//     client.log.logAndReply(message, `Timer atual da subathon do MeiaUm: ${timeLeft} ${emote}`);
+//     return;
+// };
+
+// meiaUmCommand.commandName = 'meiaum';
+// meiaUmCommand.aliases = ['omeiaum', 'meiaum', 'meiaumtimer'];
+// meiaUmCommand.shortDescription = 'Veja quanto tempo de live o meiaum ainda tem pela frente';
+// meiaUmCommand.cooldown = 5000;
+// meiaUmCommand.whisperable = true;
+// meiaUmCommand.description = ``;
+// meiaUmCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/commands/${meiaUmCommand.commandName}/${meiaUmCommand.commandName}.js`;
+
+// module.exports = {
+//     meiaUmCommand,
+// };
