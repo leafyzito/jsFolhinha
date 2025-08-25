18da26dc7d6d0963d73d7b3b39a58a7165321845
leafyzito <rafaelzao2003@gmail.com>
2025-08-25 20:06:09 +0000

    tweak(clearChat, utils): remove useless stuff

--- src/clients/twitch/events/clearChat.js
+++ src/clients/twitch/events/clearChat.js
@@ -1,12 +1,5 @@
 module.exports = function onClearChat(message) {
   if (message.targetUsername === process.env.BOT_USERNAME) {
-    console.log(
-      `* ${
-        message.isTimeout()
-          ? `Tomei timeout em #${message.channelName} por ${message.banDuration} segundos`
-          : `Fui banido em #${message.channelName}`
-      }`
-    );
     fb.discord.importantLog(
       `* ${
         message.isTimeout()
