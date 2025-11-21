70bdcb8af4d842825f1f4dbb531d2343a4e1857d
leafyzito <rafaelzao2003@gmail.com>
2025-11-21 18:16:30 -0100

    fix(percentagem): replace <> for {} in description

--- src/commands/percentagem/percentagem.js
+++ src/commands/percentagem/percentagem.js
@@ -14,8 +14,10 @@ percentagemCommand.cooldown = 5000;
 percentagemCommand.cooldownType = "channel";
 percentagemCommand.whisperable = true;
 percentagemCommand.description =
-  "Uso: !% <quantidade>; Resposta esperada: {percentagem aleatória}";
-percentagemCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/src/commands/${__dirname.split(path.sep).pop()}/${__filename.split(path.sep).pop()}`;
+  "Uso: !% {quantidade}; Resposta esperada: {percentagem aleatória}";
+percentagemCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/src/commands/${__dirname
+  .split(path.sep)
+  .pop()}/${__filename.split(path.sep).pop()}`;
 
 module.exports = {
   percentagemCommand,
